f79029bb6ad7dbd88df20a5aee1d0ffbf1f6e95e
jon-smith <jonsmith24601@outlook.com>
2020-08-22 15:57:58 +0100

    Updates

---
+++ precache-manifest.803fd006edcdbb18d8a2d63c046c2239.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "007badd78477044398bbef31d299eb08",
+    "url": "/react-ts-wasm-template/eb6465126593f7b766e8.worker.js"
+  },
+  {
+    "revision": "ee7e01d817d4f8b3164d",
+    "url": "/react-ts-wasm-template/fd30f95c0ef4be195ad9.module.wasm"
+  },
+  {
+    "revision": "f02bd6fc2366f2262ce223c686675f05",
+    "url": "/react-ts-wasm-template/index.html"
+  },
+  {
+    "revision": "9ba91d9b54915c787dc5",
+    "url": "/react-ts-wasm-template/static/css/main.47c8e558.chunk.css"
+  },
+  {
+    "revision": "2781a2159693f1397b21",
+    "url": "/react-ts-wasm-template/static/js/2.d2bef7c6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-ts-wasm-template/static/js/2.d2bef7c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee7e01d817d4f8b3164d",
+    "url": "/react-ts-wasm-template/static/js/3.e9be696d.chunk.js"
+  },
+  {
+    "revision": "9ba91d9b54915c787dc5",
+    "url": "/react-ts-wasm-template/static/js/main.2dc1d22c.chunk.js"
+  },
+  {
+    "revision": "31db096bc42447010439",
+    "url": "/react-ts-wasm-template/static/js/runtime-main.4607e556.js"
+  }
+]);
